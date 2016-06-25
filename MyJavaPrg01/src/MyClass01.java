0dce718868ea4f165e822c4151a5efed290cb25b
user01 <user01@H61M>
2016-06-25 18:47:29 +0900

    最初のコミット

---
+++ MyJavaPrg01/src/MyClass01.java
@@ -0,0 +1,13 @@
+
+public class MyClass01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Heelo,Java!");
+		System.out.println(args[0]);
+	}
+
+}
